b3ba0ff0b20b8323c3e63663bf5a7aac7b868f8d
Durohombredeez7 <53243866+Durohombredeez7@users.noreply.github.com>
2019-12-11 15:35:42 -0800

    update

---
+++ main.js
@@ -0,0 +1,61 @@
+//  get game to start once the button is clicked
+// let start = document.querySelector('.start')
+// start.addEventListener('click', () => {
+//   console.log('Game started')
+// })
+const start = document.getElementById('start');
+
+start.addEventListener('clicked', () => {
+  console.log("Kick Off");
+})
+
+// declare the player
+// let kicker = 0
+
+// let shotPlacement = document.getElementsByClassName("goal");
+// let shotPlacement = ["one", "two", "four", "five"];
+
+// register clicks on certain divs(ball placement)
+// document.querySelectorAll("goal")
+
+
+
+
+// register divs ()  listen for click
+// kicker.addEventListener('click',() => {
+//   console.log('shot is placed');
+// })
+
+
+// try console.log(shotPlacement[]); to see if console is up
+
+
+
+
+// setup positions for kicker
+// let keeperPosition = ["one", "two", "four", "five"];
+
+
+
+
+// function onClick = () => {
+// document.addEventListener('click')
+// }
+
+
+
+
+
+
+
+// setup positions for the ball(where it start off, might scrap this, functionality > presentation)
+  
+// set up collision between the goalie and ball to count as save(set values with have to be equal)
+// can potientially set up a boolean for this
+//   for (let i = 0; i < shotPlacement.length; i++) {
+//     if (kicker.shotPlacement[0] === goalie.keeperPosition[0])
+//       console.log('Saved')
+// }
+
+
+// set up random positions for goal(i can use a math.random function i practiced on pg 47 in my NB)
